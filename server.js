9c9987530aae54b27786c68909563431af0a2311
Bhanu Prakash <bhanu.putta1999@gmail.com>
2021-10-26 11:06:25 +0530

    Updated Code

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ console.log('Before');
 getUser(1, displayUser);
 console.log('After');
 
+// Consume the promises
+const userPromise = getUser(1);
+userPromise.then(user => {
+    console.log(user);
+});
+
+
 function displayCommits(commits){
         console.log("Commits", commits);
 }
@@ -16,22 +23,28 @@ function displayUser(getuser){
     getRepositories(getuser.username, displayRepositories);
 }
 
-function getUser(id, callback){
-    setTimeout(() => {
-        console.log('Reading Database');
-        callback({id: id, username: "bprakashputta"});
-    }, 2000);
+function getUser(id){
+    return new Promise((resolve, reject)=>{
+        setTimeout(() => {
+            console.log('Reading Database');
+            resolve({id: id, username: "bprakashputta"});
+        }, 2000);
+    });
 }
 
-function getRepositories(username, callback) {
-    console.log("GitHub Username: "+ username);
-    // Get list of all repositories by making get request to GitHub.
-    repos = [{name:"repo1", commits:[{id: "1",name:"cm1"},{id: "2",name:"cm2"}] },{name:"repo2", commits:"commit2"}, {name:"repo3", commits:"commit3"}];
-    callback(repos);
+function getRepositories(username) {
+    return new Promise((resolve, reject)=>{
+        console.log("GitHub Username: "+ username);
+        // Get list of all repositories by making get request to GitHub.
+        repos = [{name:"repo1", commits:[{id: "1",name:"cm1"},{id: "2",name:"cm2"}] },{name:"repo2", commits:"commit2"}, {name:"repo3", commits:"commit3"}];
+        resolve(repos);
+    });
 }
 
-function getCommits(repository, callback) {
-    console.log("Repository Name: ", repository.name);
-    commits = repository.commits;
-    callback(commits);
+function getCommits(repository) {
+    return new Promise((resolve, reject) => {
+        console.log("Repository Name: ", repository.name);
+        commits = repository.commits;
+        resolve(commits);
+    });
 }
